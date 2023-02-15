97dc5ff43a59b85d32b49cb2742827c4e6727e88
TemaisgameNoobas <60001674+TemaisgameNoobas@users.noreply.github.com>
2023-02-16 01:03:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,15 @@
 # Server version made by TemaisgameNoobas
 import cherrypy
 import os
-
-# Config
-app_port = 9089
-allow_outside_connections = False
-app_listenip = None # Leave this as "None" if you don't know what this is
-
+import config
+app_port = config.app_port
+allow_outside_connections = config.allow_outside_connections
+app_listenip = config.app_listenip
 class Main(object):
 	@cherrypy.expose
 	def index(self):
 		pass
-
 conf = { '/':{ 'tools.staticdir.on' : True,'tools.staticdir.root' : os.path.abspath(os.path.join(os.path.dirname(__file__))),'tools.staticdir.dir' : os.path.abspath(os.path.join(os.path.dirname(__file__), 'www')),'tools.staticdir.index' : 'index.html', } }
-
 cherrypy.config.update({'server.socket_port': app_port})
 bindip = None
 if app_listenip == None:
@@ -25,6 +21,5 @@ if app_listenip == None:
 			bindip = '127.0.0.1'
 elif not app_listenip == None:
 	bindip = app_listenip
-
 cherrypy.config.update({'server.socket_host': bindip})
 cherrypy.quickstart(Main(),config=conf)
